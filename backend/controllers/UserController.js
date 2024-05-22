a6810a32942defd07610c5e0d934eac2d24216ce
happy-harsh <harsh.dodi@gmail.com>
2024-05-22 21:53:43 +0530

    sameSite none

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -63,7 +63,7 @@ const handleLoginUser = async (req, res) => {
     {
       path: "/",
       httpOnly: true,
-      sameSite: "lax",
+      sameSite: "none",
       secure: true,
       domain: "shopezy-mern-backend.vercel.app"
     }
@@ -93,7 +93,7 @@ const handleLogoutUser = (req,res) => {
           {
             path: "/",
             httpOnly: true,
-            sameSite: "lax",
+            sameSite: "none",
             secure: true,
             domain: "shopezy-mern-backend.vercel.app"
           }
